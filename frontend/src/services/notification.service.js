fd6be4bc1bfa72f156005c9cd12680d50ac9511f
Dhanush <dhanushcse00@gmail.com>
2026-05-25 11:27:42 +0530

    Delete frontend directory

--- frontend/src/services/notification.service.js
+++
@@ -1,9 +0,0 @@
-import api from './api.js';
-
-const notificationService = {
-  getAll:     ()   => api.get('/notifications'),
-  markRead:   (id) => api.patch(`/notifications/${id}/read`),
-  markAllRead:()   => api.patch('/notifications/read-all'),
-};
-
-export default notificationService;
